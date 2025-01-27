22b8827c48a57ec62f81964e0815e61a8e96f3ff
Andressa Sousa <andressa_sousaf@outlook.com>
2025-01-27 18:57:52 -0300

    Atualizando frame_1.h

--- frame_1.h
+++ frame_1.h
@@ -12,7 +12,6 @@
 #include "hardware/adc.h"
 #include "pico/bootrom.h"
 
-
 #define NUM_PIXELS 25
 
 // Definição de tipo da estrutura que irá controlar a cor dos LED's
@@ -20,15 +19,17 @@ typedef struct {
     double red;
     double green;
     double blue;
-}Led_config;
 
-typedef Led_config RGB_cod;
+}Led_config1;
+
+typedef Led_config1 RGB_cod1;
 
 // Definição de tipo da matriz de leds
-typedef Led_config Matriz_leds_config[5][5];
+typedef Led_config1 Matriz_leds_config1[5][5];
 
 //função para definição da intensidade de cores do led
-uint32_t matrix_rgb(double b, double r, double g)
+uint32_t matrixrgb1(double b, double r, double g)
+
 {
   unsigned char R, G, B;
   R = r * 255;
@@ -37,11 +38,18 @@ uint32_t matrix_rgb(double b, double r, double g)
   return (G << 24) | (R << 16) | (B << 8);
 }
 
-void imprimir_desenho(Matriz_leds_config configuracao, PIO pio, uint sm){
+void controlar_leds(PIO pio, uint sm, uint32_t cor)
+{
+  for (int16_t i = 0; i < NUM_PIXELS; i++) {
+    pio_sm_put_blocking(pio, sm, cor);
+  }
+}
+
+void imprimir_desenho(Matriz_leds_config1 configuracao, PIO pio, uint sm){
     for (int contadorLinha = 4; contadorLinha >= 0; contadorLinha--){
         if(contadorLinha % 2){
             for (int contadorColuna = 0; contadorColuna < 5; contadorColuna ++){
-                uint32_t valor_cor_binario = matrix_rgb(
+                uint32_t valor_cor_binario = matrixrgb1(
                     configuracao[contadorLinha][contadorColuna].blue,
                     configuracao[contadorLinha][contadorColuna].red,
                     configuracao[contadorLinha][contadorColuna].green
@@ -51,7 +59,7 @@ void imprimir_desenho(Matriz_leds_config configuracao, PIO pio, uint sm){
             }
         }else{
             for (int contadorColuna = 4; contadorColuna >= 0; contadorColuna --){
-                uint32_t valor_cor_binario = matrix_rgb(
+                uint32_t valor_cor_binario = matrixrgb1(
                     configuracao[contadorLinha][contadorColuna].blue,
                     configuracao[contadorLinha][contadorColuna].red,
                     configuracao[contadorLinha][contadorColuna].green
@@ -63,32 +71,33 @@ void imprimir_desenho(Matriz_leds_config configuracao, PIO pio, uint sm){
     }
 }
 
-RGB_cod obter_cor_por_parametro_RGB(int red, int green, int blue){
-    RGB_cod cor_customizada = {red/255.0,green/255.0,blue/255.0};
+RGB_cod1 cor_parametro (double red, double green, double blue){
+    RGB_cod1 cor_customizada = {red/255.0,green/255.0,blue/255.0};
 
     return cor_customizada;
 }
 
-void animacaoletraA(PIO pio, uint sm){
-
-    //Definição de cores usadas na intensidade adequada para a placa
-
-        /*
-        RGB_cod verde = obter_cor_por_parametro_RGB(0,7,0);
-        RGB_cod marromClaro = obter_cor_por_parametro_RGB(9.375,4.6875,0);
-        RGB_cod marrom = obter_cor_por_parametro_RGB(4.6875,2.34375,0);
-        RGB_cod verde_claro = obter_cor_por_parametro_RGB(0.0,2,0);
-        */
+void frame_arvore(PIO pio, uint sm, float intsty){
 
     //Definição de cores usadas na intensidade adequada para o simulador
     
-    RGB_cod verde = obter_cor_por_parametro_RGB(0.0,510,0.0);
-    RGB_cod marromClaro = obter_cor_por_parametro_RGB(126,76,0);
-    RGB_cod marrom = obter_cor_por_parametro_RGB(150,75,0);
-    RGB_cod verde_claro = obter_cor_por_parametro_RGB(0,100,0);
+    RGB_cod1 verde = cor_parametro(0.0,510,0.0);
+    RGB_cod1 marromClaro = cor_parametro(126,76,0);
+    RGB_cod1 marrom = cor_parametro(150,75,0);
+    RGB_cod1 verde_claro = cor_parametro(0,100,0);
+
+    if (intsty == 1){
+        //Definição de cores usadas na intensidade adequada para a placa
+        verde = cor_parametro(0,7,0);
+        marromClaro = cor_parametro(9.375,4.6875,0);
+        marrom = cor_parametro(4.6875,2.34375,0);
+        verde_claro = cor_parametro(0.0,2,0);
+    };
     
 
-    Matriz_leds_config frames[] ={
+    
+    //Matriz com todos os frames da animação em ordem - Animação é uma árvore subindo e intensificando de cor
+    Matriz_leds_config1 frames[] ={
         //frame 0 arvore subindo linha 1
         {{{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}},{{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, {{0.0, 0.0, 0.0}, verde_claro, verde_claro, verde_claro, {0.0, 0.0, 0.0}} },
         //frame 1 arvore subindo linha 2
@@ -115,10 +124,10 @@ void animacaoletraA(PIO pio, uint sm){
         {{{0.0, 0.0, 0.0}, verde, verde, verde, {0.0, 0.0, 0.0}}, {verde, verde, verde, verde, verde},{verde, verde, verde, verde, verde}, {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, marrom, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, marromClaro, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}},
         //frame 11
         {{{0.0, 0.0, 0.0}, verde, verde, verde, {0.0, 0.0, 0.0}}, {verde, verde, verde, verde, verde},{verde, verde, verde, verde, verde}, {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, marrom, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}, {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, marrom, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}}    
-        
     };
+    
 
-    //For para apresentar todos os 35 frames em ordem com intervalo de 250ms (4fps)
+    //For para apresentar todos os 12 frames em ordem com intervalo de 250ms (4fps)
     for(int i=0;i<12;i++){
             //imprimir os frames 
             imprimir_desenho(frames[i],pio,sm);
@@ -126,8 +135,5 @@ void animacaoletraA(PIO pio, uint sm){
     };
 
 };
-
-
-
 
 #endif
